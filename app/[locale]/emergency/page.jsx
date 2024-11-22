a25ad5a1bf5bee74fd507bd8ab02ae9a58df97ee
Jimena Hernández <her21199@uvg.edu.gt>
2024-11-22 11:12:13 -0600

    update tabcart

--- app/[locale]/emergency/page.jsx
+++ app/[locale]/emergency/page.jsx
@@ -19,16 +19,16 @@ export default async function HouseKeeping({ params: { locale }}) {
                 <HeaderItem v={"v4"} transparent/>
                 <h2 className='italictiempos_title text-quaternary'>{t('emergency:title')}</h2>
                 <InfoDisplay collection="Emergency" colorlines="quaternary" coloricon="tertiary"/>
-                <div className="flex flex-col justify-center items-center gap-4 md:w-3/4 px-10 w-full">
+                <div className="flex flex-col justify-center items-center gap-4 md:w-3/4 px-10 w-full h-60 md:h-full">
                     <div className='collapse-title md:text-lg text-sm font-medium font-futura uppercase peer-checked:collapse-open text-quaternary py-0'>
                         Emergency Contacts
                     </div>
-                    <div className='flex md:flex-row flex-col md:justify-center justify-start items-center md:p-10 md:mx-0 mx-auto border-black border-b w-full'>
-                        <div className='flex justify-start items-center md:h-48 w-full '>
+                    <div className='flex md:flex-row flex-col md:justify-center justify-start items-center mx-auto border-black border-b w-full'>
+                        <div className='flex justify-start items-center w-full '>
                             <PhoneItem name={t('emergency:phone1')} p1="+501 803-2038;" link1="tel:+501 803-2038" />
                             <PhoneItem name={t('emergency:phone2')} p1="+501 804-2022" link1="tel:+501 804-2022" />
                         </div>
-                        <div className='flex  justify-start items-center md:h-48 w-full'>
+                        <div className='flex  justify-start items-center w-full'>
                             <PhoneItem name={t('emergency:phone3')} p1="+501 824-2066" link1="tel:+501 824-2066" />
                             <PhoneItem name={t('emergency:phone4')} p1="+501 635-0678" link1="tel:+501 635-0678" />
                         </div>
