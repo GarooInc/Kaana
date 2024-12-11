648a47ed99e4408a71daee95376b688d2478a464
Jimena Hernández <her21199@uvg.edu.gt>
2024-12-11 11:15:55 -0600

    Update page.jsx

--- app/[locale]/amenities/page.jsx
+++ app/[locale]/amenities/page.jsx
@@ -14,11 +14,11 @@ export default async function Amenities({ params: { locale }}) {
 
 return (
     <TranslationsProvider locale={locale} namespaces={namespaces} resources={resources}>
-        <div className="bg-tertiary page">
+        <div className="bg-tertiary page md:justify-start justify-between">
             <HeaderItem v={"v12"} transparent whiteArrow/>
             <h1 className="principal_title italic">{t('amenities:title')}</h1>
             <AmenitiesItem/>
-            <div className='w-full justify-center flex items-center py-10'>
+            <div className='w-full justify-center flex items-center py-10 md:absolute md:bottom-0'>
                 <img src="/assets/images/logo_v9.png" alt="logo" className="w-[80px]" />
             </div>
         </div>
